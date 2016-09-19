0843b9a9070440c8da3914f835768f43d3f9b62f
Michael Herman <hermanmu@gmail.com>
2016-09-19 16:40:37 -0600

    added cache

--- cookbook/views.py
+++ cookbook/views.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from .services import get_recipes_without_cache as get_recipes
+from .services import get_recipes_with_cache as get_recipes
 
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 
+@cache_page(CACHE_TTL)
 def recipes_view(request):
     return render(request, 'cookbook/recipes.html', {
         'recipes': get_recipes()
--- example/settings.py
+++ example/settings.py
@@ -136,3 +136,6 @@ CACHES = {
         "KEY_PREFIX": "example"
     }
 }
+
+# Cache time to live is 15 minutes.
+CACHE_TTL = 60 * 15
